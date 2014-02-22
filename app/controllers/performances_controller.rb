6bd21711bd5f4525311b01a7be29359de48e7631
takashi <kikaihonyaku@gmail.com>
2014-02-23 00:05:44 +0900

    ググラフの取得方法を改革

--- app/controllers/performances_controller.rb
+++ app/controllers/performances_controller.rb
@@ -19,6 +19,10 @@ class PerformancesController < ApplicationController
 
   # 月次情報
   def monthly
+
+    # 戻り値のHashを格納する為の配列
+    @result_arr = []
+
     #################
     # 年度指定
     #################
@@ -32,214 +36,52 @@ class PerformancesController < ApplicationController
     #  項目CDの先頭には２文字、ID重複しないためのキーが入っているので、それを除いて使用
     item = Item.find_by_code(params[:item].slice(2, params[:item].length - 2))
 
-    # 集計種別を取得 1:合計 2:平均 3:最大
-    p "集計種別：" + params[:item_summary]
-    case params[:item_summary].to_i
-    when 1
-      func_summary = "SUM"
-    when 2
-      func_summary = "AVG"
-    when 3
-      func_summary = "SUM" # だたしこの時は年計は出さない
-    end
+    # params[:item_summary]    ←集計種別  1:合計 2:平均 3:最大
+    item_summary = params[:item_summary]
+
 
     #################
     # 部署判定
     #################
-    busyo = params[:dept].to_i
+#    busyo = params[:dept].to_i
+    params[:dept_list].split(",").each do |busyo|
 
-    dept_arr = []
-    dept_group = DeptGroup.find_by_busyo_id(busyo)
-    if dept_group
-      # グループ部署の時
-      dept_name = dept_group.name
-      dept_group.dept_group_details.each do |detail|
-        dept_arr.push(detail.dept_id)
-      end
-    else
-      # 通常部署の時
-      dept = Dept.find_by_busyo_id(busyo)  # ビル管理事業部
-      dept_name = dept.name
-      dept_arr.push(dept)
-    end
-
-
-    #################
-    # スコープ定義
-    #################
-    monthly = MonthlyStatement.scoped
-    monthly = monthly.where(["dept_id In (?)", dept_arr])
-    monthly = monthly.where("item_id = " + item.id.to_s)
-
-    # 今年度の計画実績
-    this_year_monthly = monthly.where("yyyymm>=" + yyyymm_s)
-    this_year_monthly = this_year_monthly.where("yyyymm<=" + yyyymm_e)
-    this_year_monthly = this_year_monthly.group("yyyymm").select(func_summary + "(plan_value) as plan_value," + func_summary +" (result_value) as result_value, yyyymm")
-
-    ##################################
-    # 今年度の計画／実績と、昨年の実績を取得
-    ##################################
-    @categories = []
-    @this_year_plans = []
-    @this_year_results = []
-    @prev_year_results = []
-
-    plan_exists = false # 計画が定義されているか判定する。
-
-    this_year_monthly.each do |rec|
-
-      # 今年度の計画／実績
-      #categories.push(rec.yyyymm.slice(4..5).to_i.to_s + "月")
-      @categories.push(rec.yyyymm)
-      @this_year_plans.push(rec.plan_value.to_f)
-      @this_year_results.push(rec.result_value.to_f)
-
-      # 計画が一つでも登録されていれば、計画棒グラフを出す。
-      plan_exists = true if rec.plan_value.to_f > 0
-
-      # 前年実績
-      prev_year = rec.yyyymm.slice(0..3).to_i - 1
-      prev_year_monthly = monthly.where("yyyymm = " + prev_year.to_s + rec.yyyymm.slice(4..5))
-      prev_year_monthly = prev_year_monthly.group("yyyymm").select(func_summary + "(plan_value) as plan_value, " + func_summary + "(result_value) as result_value, yyyymm")
-
-      reg_flg = false
-      prev_year_monthly.each do |rec2|
-        @prev_year_results.push(rec2.result_value.to_f)
-        reg_flg = true
-      end
-
-      unless reg_flg
-        @prev_year_results.push(0)
-      end
+      # ここで指定した条件で以下のkeyを持つ配列が返ってくるので、それを配列に格納
+      # Hashのkey
+      # result['categories'] 　　　　・・・・　年月のカテゴリ配列
+      # result['this_year_plans'] 　・・・・　計画値の配列
+      # result['this_year_results'] ・・・・　実績値の配列
+      # result['prev_year_results'] ・・・・　前年実績値の配列
+      # result['graph_plan'] ・・・・　計画／実績／前年実績の棒グラフ
+      # result['graph_years'] ・・・・ 年計グラフ
+      @result_arr.push(get_monthly_graph(busyo.to_i, yyyymm_s, yyyymm_e, item, item_summary, yyyy.to_s + '年度'))
 
     end
 
-    ##################################
-    # 計画／実績の棒グラフを作成(前年対比も含める)
-    ##################################
-    @plan_result = LazyHighCharts::HighChart.new('graph') do |f|
-#      f.chart(:type => "spline")
-#      f.chart(:type => "column")
-      f.title(text: dept_name + 'の' + item.name + '(' + yyyy.to_s + '年度)')
-      f.xAxis(categories: @categories.collect do |ym| ym.slice(4..5).to_i.to_s + "月" end, tickInterval: 1) # 1とかは列の間隔の指定
-      f.series(name: (yyyy -1).to_s + '年度実績', data: @prev_year_results, type: "column", color: '#8cc63f')
-
-      if plan_exists
-        f.series(name: '計画', data: @this_year_plans, type: "column", color: '#3276b1')
-      end
-
-      f.series(name: '実績', data: @this_year_results, type: "column", color: '#d9534f')
-    end
-
-    ##################################
-    # 実績の前年対比
-    ##################################
-#    @year_on_year = LazyHighCharts::HighChart.new('graph') do |f|
-#      f.title(text: dept_name + 'の' + item.name + '(昨年対比)')
-#      f.xAxis(categories: categories.collect do |ym| ym.slice(4..5).to_i.to_s + "月" end, tickInterval: 1) # 1とかは列の間隔の指定
-#      f.series(name: (yyyy -1).to_s + '年度実績', data: prev_year_results, type: "column", color: '#8cc63f')
-#      f.series(name: yyyy.to_s + '年度実績', data: this_year_results, type: "column", color: '#d9534f')
-#    end
-
-    ############################################################################
-    # 集計種別が「最大」でない時、年計グラフを作成（管理戸数の累計などだしても意味が無いから）
-    ############################################################################
-    # 指定した月で最小の月+12ヶ月を取得する。
-    if params[:item_summary].to_i != 3
-      min_month = nil
-      max_month = nil
-      year_sum = monthly.select("MIN(yyyymm) as yyyymm_min, MAX(yyyymm) as yyyymm_max").where("result_value > 0")
-      year_sum.each do |rec|
-        min_month = rec.yyyymm_min
-        max_month = rec.yyyymm_max
-      end
-
-      if min_month
-        dt_start = Date.parse(min_month + '01') # 最小月
-        dt_end = dt_start >> 11 # ＋11ヶ月後
-        dt_max = Date.parse(max_month + '01') # 最大月
-
-        # データが12ヶ月以上ある場合、年計グラフを作成する
-        if dt_end <= dt_max
-
-          year_category = []
-          year_result = []
-
-          while dt_end <= dt_max
-
-            #p dt_end.strftime("%Y%m")
-
-            year_category.push(dt_end.strftime("%Y%m"))
-
-            year_sum_v2 = monthly.where("yyyymm between ? and ?", dt_start.strftime("%Y%m"), dt_end.strftime("%Y%m") ).select(func_summary + "(result_value) as result_value")
-            year_sum_v2.each do |rec|
-              p rec.result_value
-              year_result.push(rec.result_value.to_f)
-            end
-
-            # 1ヶ月進める
-            dt_start = dt_start >> 1
-            dt_end = dt_end >> 1
-
-
-          end
-
-          interval = ((year_category.length)/12).to_i + 1
-
-          @year_result = LazyHighCharts::HighChart.new('graph') do |f|
-            f.title(text: dept_name + 'の' + item.name + 'の年計')
-            f.xAxis(categories: year_category, tickInterval: interval) # 1とかは列の間隔の指定
-            f.series(name: '実績', data: year_result, type: "column")
-          end
-
-        end
-
-      end
-
-    end
 
     ##############################################
     # グループ部署の時、子部署の実績を線グラフで表示する。
     ##############################################
-    if dept_group
-      @group_result = LazyHighCharts::HighChart.new('graph') do |f|
-        f.title(text: dept_name + 'の' + item.name + 'の実績一覧')
-        f.xAxis(categories: @categories.collect do |ym| ym.slice(4..5).to_i.to_s + "月" end, tickInterval: 1) # 1とかは列の間隔の指定
-        #f.series(name: '実績', data: this_year_results, type: "spline")
+   @group_result = LazyHighCharts::HighChart.new('graph') do |f|
+     f.title(text: 'dept_name' + 'の' + item.name + 'の実績一覧')
+     f.xAxis(categories: @result_arr[0]['categories'].collect do |ym| ym.slice(4..5).to_i.to_s + "月" end, tickInterval: 1) # 1とかは列の間隔の指定
+     #f.series(name: '実績', data: this_year_results, type: "spline")
 
-        # 凡例
-        f.legend(
-            layout: 'vertical',
-            backgroundColor: '#FFFFFF',
-            floating: true,
-            align: 'right',
-            x: 0,
-            verticalAlign: 'top',
-            y: 0
-        )
+     # 凡例
+     f.legend(
+         layout: 'vertical',
+         backgroundColor: '#FFFFFF',
+         floating: true,
+         align: 'right',
+         x: 0,
+         verticalAlign: 'top',
+         y: 0
+     )
 
-        dept_arr.each do |dept_id|
-          group_result = []
-          dept = Dept.find(dept_id)
-          p dept.name
-
-          @categories.each do |yymm|
-            monthly_result = MonthlyStatement.scoped
-            monthly_result = monthly_result.where("item_id = " + item.id.to_s)
-            monthly_result = monthly_result.where("yyyymm=" + yymm)
-            monthly_result = monthly_result.where("dept_id = " +  dept.id.to_s)
-            #monthly_result = monthly_result.group("yyyymm").select("SUM(plan) as plan, SUM(result) as result, yyyymm")
-
-            monthly_result.each do |rec|
-              group_result.push(rec.result_value.to_f)
-            end
-          end
-
-#          f.series(name: dept.to_s , data: group_result.dup, type: "spline")
-          f.series(name: dept.name , data: group_result.dup, type: "line")
-        end
-      end
-    end # if
+     @result_arr.each_with_index do |result, i|
+       f.series(name: i.to_s , data: result['this_year_results'].dup, type: "line")
+     end
+   end
     
   end
 
@@ -509,4 +351,157 @@ private
 
   end
 
+  # 指定された部署／項目／年月のグラフを取得する
+  def get_monthly_graph(dept_id, yyyymm_from, yyyymm_to, item, item_summary, graph_title)
+
+    ##################
+    # 部署の展開
+    ##################
+    dept_arr = []
+    dept_group = DeptGroup.find_by_busyo_id(dept_id)
+    if dept_group
+      # グループ部署の時
+      dept_name = dept_group.name
+      dept_group.dept_group_details.each do |detail|
+        dept_arr.push(detail.dept_id)
+      end
+    else
+      # 通常部署の時
+      dept = Dept.find_by_busyo_id(dept_id)
+      dept_name = dept.name
+      dept_arr.push(dept)
+    end
+
+    #################
+    # スコープ定義
+    #################
+    monthly = MonthlyStatement.scoped
+    monthly = monthly.where(["dept_id In (?)", dept_arr])
+    monthly = monthly.where("item_id = " + item.id.to_s)
+
+    case item_summary.to_i
+    when 1
+      func_summary = "SUM"
+    when 2
+      func_summary = "AVG"
+    when 3
+      func_summary = "SUM" # だたしこの時は年計は出さない
+    end
+
+    # 今年度の計画実績
+    this_year_monthly = monthly.where("yyyymm>=" + yyyymm_from)
+    this_year_monthly = this_year_monthly.where("yyyymm<=" + yyyymm_to)
+    this_year_monthly = this_year_monthly.group("yyyymm").select(func_summary + "(plan_value) as plan_value," + func_summary +" (result_value) as result_value, yyyymm")
+
+    ##################################
+    # 今年度の計画／実績と、昨年の実績を取得
+    ##################################
+    result = {}
+    result['categories'] = []
+    result['this_year_plans'] = []
+    result['this_year_results'] = []
+    result['prev_year_results'] = []
+
+    plan_exists = false # 計画が定義されているか判定する。
+    this_year_monthly.each do |rec|
+
+      # 今年度の計画／実績
+      result['categories'].push(rec.yyyymm)
+      result['this_year_plans'].push(rec.plan_value.to_f)
+      result['this_year_results'].push(rec.result_value.to_f)
+
+      # 計画が一つでも登録されていれば、計画棒グラフを出す。
+      plan_exists = true if rec.plan_value.to_f > 0
+
+      # 前年実績
+      prev_year = rec.yyyymm.slice(0..3).to_i - 1
+      prev_year_monthly = monthly.where("yyyymm = " + prev_year.to_s + rec.yyyymm.slice(4..5))
+      prev_year_monthly = prev_year_monthly.group("yyyymm").select(func_summary + "(plan_value) as plan_value, " + func_summary + "(result_value) as result_value, yyyymm")
+
+      reg_flg = false
+      prev_year_monthly.each do |rec2|
+        result['prev_year_results'].push(rec2.result_value.to_f)
+        reg_flg = true
+      end
+
+      unless reg_flg
+        result['prev_year_results'].push(0)
+      end
+
+    end
+
+    ##################################
+    # 計画／実績の棒グラフを作成(前年対比も含める)
+    ##################################
+    result['graph_plan'] = LazyHighCharts::HighChart.new('graph') do |f|
+      f.title(text: dept_name + 'の' + item.name + '(' + graph_title + ')')
+      f.xAxis(categories: result['categories'].collect do |ym| ym.slice(4..5).to_i.to_s + "月" end, tickInterval: 1) # 1とかは列の間隔の指定
+      f.series(name: '前年実績', data: result['prev_year_results'], type: "column", color: '#8cc63f')
+
+      if plan_exists
+        f.series(name: '計画', data: result['this_year_plans'], type: "column", color: '#3276b1')
+      end
+
+      f.series(name: '実績', data: result['this_year_results'], type: "column", color: '#d9534f')
+    end
+
+    ############################################################################
+    # 集計種別が「最大」でない時、年計グラフを作成（管理戸数の累計などだしても意味が無いから）
+    ############################################################################
+    # 指定した月で最小の月+12ヶ月を取得する。
+    if item_summary.to_i != 3
+      min_month = nil
+      max_month = nil
+      year_sum = monthly.select("MIN(yyyymm) as yyyymm_min, MAX(yyyymm) as yyyymm_max").where("result_value > 0")
+      year_sum.each do |rec|
+        min_month = rec.yyyymm_min
+        max_month = rec.yyyymm_max
+      end
+
+      if min_month
+        dt_start = Date.parse(min_month + '01') # 最小月
+        dt_end = dt_start >> 11 # ＋11ヶ月後
+        dt_max = Date.parse(max_month + '01') # 最大月
+
+        # データが12ヶ月以上ある場合、年計グラフを作成する
+        if dt_end <= dt_max
+
+          year_category = []
+          year_result = []
+
+          while dt_end <= dt_max
+
+            #p dt_end.strftime("%Y%m")
+
+            year_category.push(dt_end.strftime("%Y%m"))
+
+            year_sum_v2 = monthly.where("yyyymm between ? and ?", dt_start.strftime("%Y%m"), dt_end.strftime("%Y%m") ).select(func_summary + "(result_value) as result_value")
+            year_sum_v2.each do |rec|
+              year_result.push(rec.result_value.to_f)
+            end
+
+            # 1ヶ月進める
+            dt_start = dt_start >> 1
+            dt_end = dt_end >> 1
+
+          end
+
+          interval = ((year_category.length)/12).to_i + 1
+
+          result['graph_years'] = LazyHighCharts::HighChart.new('graph') do |f|
+            f.title(text: dept_name + 'の' + item.name + 'の年計')
+            f.xAxis(categories: year_category, tickInterval: interval) # 1とかは列の間隔の指定
+            f.series(name: '実績', data: year_result, type: "column")
+          end
+
+        end
+
+      end
+    end
+
+    return result
+
+  end
+
 end
+
