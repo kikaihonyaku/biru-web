63b0249381182c15dc4f69f65837999ec70e361f
柴田貴史 <shibatatakashi@shibatatakashi-no-iMac.local>
2014-07-28 06:24:30 +0900

    レンターズ連動テスト実施

--- app/controllers/trust_managements_controller.rb
+++ app/controllers/trust_managements_controller.rb
@@ -5,50 +5,7 @@ require 'kconv'
 class TrustManagementsController < ApplicationController
   
   before_filter :search_init
-  
-  def tack_out
-    require 'thinreports'
     
-    @selected = params[:g1][:selected]
-    # @trusts = Trust.joins(:building => :shop ).joins(:owner).joins(:manage_type).where("trusts.id in (?)", @selected)
-    #
-    # aaa = []
-    # @trusts.each do |t|
-    #   aaa.push(t.building.name) if t.building
-    # end
-    
-    owner_id_arr = []
-    Trust.where("id in (?)", @selected).each do |trust|
-      owner_id_arr.push(trust.owner_id)
-    end
-    
-    @owners = Owner.where("id in (?)", owner_id_arr)
-    #send_data @owners.to_csv, :filename=>'tack.csv'
-    
-    # pdfファイルを作成
-    #report = ThinReports::Report.create :layout => File.join(Rails.root, 'app/reports', 'pdf_layout.tlf') do |r|
-    report = ThinReports::Report.create :layout => File.join(Rails.root, 'app/reports', 'pdf_layout.tlf') do |r|
-       
-       @owners.each_with_index do |owner, idx|
-         
-         lbl_num = (idx).modulo(8) # 剰余を求める
-         
-         if lbl_num == 0
-           r.start_new_page
-         end
-         
-         r.page.values "name_0" + (lbl_num + 1).to_s => "テスト"
-         
-       end
-       
-     end
-
-     send_data report.generate, :filename    => 'foo.pdf', 
-                                :type        => 'application/pdf', 
-                                :disposition => 'attachment'
-     
-  end
-  
   def index
     # Owner Building Trust を連結した他社データを取得する
     @trust_data = get_trust_data
