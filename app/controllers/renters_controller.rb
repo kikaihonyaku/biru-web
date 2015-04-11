7f8a77725b1396fdfb46796ea3c7e1914fba235f
柴田貴史 <shibatatakashi@shibatatakashi-no-iMac.local>
2015-04-11 20:51:56 +0900

    レンターズで不要な写真をカウントしてしまう不具合対応

--- app/controllers/renters_controller.rb
+++ app/controllers/renters_controller.rb
@@ -343,8 +343,6 @@ class RentersController < ApplicationController
     ,notice_d
     ,notice_e
     ,notice_f
-    ,notice_g
-    ,notice_h
     ,latitude
     ,longitude
     ,J00
@@ -382,8 +380,6 @@ class RentersController < ApplicationController
     ,a.notice_d
     ,a.notice_e
     ,a.notice_f
-    ,a.notice_g
-    ,a.notice_h
     ,c.id as building_id
     ,c.address
     ,c.latitude
@@ -415,6 +411,7 @@ class RentersController < ApplicationController
     inner join renters_buildings c on a.renters_building_id = c.id 
     where a.delete_flg = 'f'
     and b.delete_flg = 'f'
+    and c.delete_flg = 'f'
     "
     
     if sakimono_flg
@@ -443,8 +440,6 @@ class RentersController < ApplicationController
     ,a.notice_d
     ,a.notice_e
     ,a.notice_f
-    ,a.notice_g
-    ,a.notice_h
     ,c.id
     ,c.address
     ,c.latitude
